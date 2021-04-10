bbe939f357f8b868e156359ddd3b77ba4f8ed87f
xix22 <b.danib@hotmail.com>
2021-04-10 13:21:37 +0100

    Initial commit

---
+++ app/src/main/java/com/group21/buggycontrol/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.group21.buggycontrol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
